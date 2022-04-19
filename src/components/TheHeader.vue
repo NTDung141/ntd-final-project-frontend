48cca51854347621ad6456822bd23035ee5caf3c
Nguyen Thanh Dung <dungynot@gmail.com>
2022-04-20 02:35:11 +0700

    fix vuetify for login form

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,48 +1,54 @@
 <template>
-  <v-toolbar class="my-toolbar">
-    <span class="hidden-sm-and-up">
-      <v-toolbar-side-icon @click="sidebar = !sidebar"> </v-toolbar-side-icon>
-    </span>
+  <div>
+    <v-toolbar class="my-toolbar">
+      <span class="hidden-sm-and-up">
+        <i class="fas fa-bars" @click="sidebar = !sidebar"></i>
+      </span>
 
-    <v-tabs class="hidden-xs-only">
-      <v-tab v-for="item in menuItems" :key="item.title" :to="item.path">
-        {{ item.title }}
-      </v-tab>
-    </v-tabs>
+      <v-tabs class="hidden-xs-only">
+        <v-tab v-for="item in menuItems" :key="item.title" :to="item.path">
+          {{ item.title }}
+        </v-tab>
+      </v-tabs>
 
-    <v-space></v-space>
+      <v-spacer></v-spacer>
 
-    <div v-if="!userInfo.email">
-      <v-btn class="mr-2" depressed color="primary"> Sign in </v-btn>
+      <div class="my-toolbar-action" v-if="!userInfo.email">
+        <v-btn class="mr-2" depressed color="primary" to="/login">
+          Sign in
+        </v-btn>
 
-      <v-btn depressed outlined color="primary"> Sign up </v-btn>
-    </div>
-
-    <div class="my-toolbar-action" v-if="userInfo.email">
-      <div class="mr-5">
-        <i class="far fa-bell fa-lg"></i>
+        <v-btn depressed outlined color="primary" to="/register">
+          Sign up
+        </v-btn>
       </div>
 
-      <div class="text-center">
-        <v-menu offset-y right>
-          <template v-slot:activator="{ on, attrs }">
-            <v-avatar size="30" v-bind="attrs" v-on="on">
-              <img src="@/assets/defaultAvatar2.jpg" />
-            </v-avatar>
-          </template>
-          <v-list>
-            <v-list-item>
-              <v-list-item-title>User Profile</v-list-item-title>
-            </v-list-item>
+      <div class="my-toolbar-action" v-if="userInfo.email">
+        <div class="mr-5">
+          <i class="far fa-bell fa-lg"></i>
+        </div>
 
-            <v-list-item @click="onLogout()">
-              <v-list-item-title>Sign out</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
+        <div class="text-center">
+          <v-menu offset-y right>
+            <template v-slot:activator="{ on, attrs }">
+              <v-avatar size="30" v-bind="attrs" v-on="on">
+                <img src="@/assets/defaultAvatar2.jpg" />
+              </v-avatar>
+            </template>
+            <v-list>
+              <v-list-item>
+                <v-list-item-title>User Profile</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item @click="onLogout()">
+                <v-list-item-title>Sign out</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
       </div>
-    </div>
-  </v-toolbar>
+    </v-toolbar>
+  </div>
 </template>
 
 <script>
@@ -70,7 +76,7 @@ export default {
         { title: "My project", path: "/my-project" },
       ],
 
-      sideBar: false,
+      sideBar: true,
 
       items: [
         { title: "Click Me" },
@@ -122,5 +128,11 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+@media only screen and (max-width: 767.98px) {
+  .my-toolbar {
+    padding: 0px;
+  }
 }
 </style>
